ef67cf80a76d32934c6d411e3954fb6e12a17fd7
Batman <47056442+Unicodist@users.noreply.github.com>
2025-06-07 20:17:36 +0545

    Added more programs

---
+++ PercentToGrade/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a percentage:");
+decimal percentage = decimal.Parse(Console.ReadLine());
+
+switch (percentage)
+{
+    case >= 90:
+        Console.WriteLine("Excellent");
+        break;
+    case >= 70:
+        Console.WriteLine("Very Good");
+        break;
+}
---
+++ ReverseTrianglePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * ___*
+ * __**
+ * _***
+ * ****
+ */
+
+for(int i = 1; i<= 20; i++)
+{
+    for(int s = 1; s <= 20 - i; s++)
+    {
+        Console.Write(" ");
+    }
+    for(int j = 1; j<= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
