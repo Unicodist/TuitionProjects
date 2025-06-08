3343235835a7fbee457d44bce77b3aba22e57db5
Batman <47056442+Unicodist@users.noreply.github.com>
2025-06-08 17:32:22 +0545

    Added more patterns

---
+++ RectangleOutlinePattern/Program.cs
@@ -0,0 +1,15 @@
+﻿for(int i = 1; i <=7; i++)
+{
+    for(int j = 1; j <=7; j++)
+    {
+        if (i == 1 || i == 7 || j == 1 || j == 7)
+        {
+            Console.Write("*");
+        }
+        else {
+            Console.Write(" ");
+        }
+        
+    }
+    Console.WriteLine();
+}
---
+++ UpsideDownTrianglePattern/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * *****
+ * ****
+ * ***
+ * **
+ * *
+ */
+
+Console.WriteLine("Enter the number of rows:");
+int rows = int.Parse(Console.ReadLine());
+
+for(int i = rows; i >= 1; i--)
+{
+    for(int j = 1; j<=i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
